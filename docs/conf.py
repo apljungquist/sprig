f0818e1ec9dc28a6d3e449511d45148895c0ba90
AP Ljungquist <ap@ljungquist.eu>
2019-01-28 21:35:16 +0100

    Customizes docs
    Includes readme on index page to make the docs not completely useless fast.
    
    Makes sphinx-apidoc run on readthedocs by adding an extension that runs it
    automatically whenever the docs are built. Not having to manually include
    modules etc in the docs should make it easier to maintain even if it will not
    be as neat.
    
    Makes readthedocs use python 3 because that is what this project targets, and
    pip install the package so that autodoc works.

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,13 @@ release = '0.0.12'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinxcontrib.apidoc',
+    'm2r',
 ]
+
+apidoc_module_dir = '../src'
+apidoc_output_dir = '.'
+apidoc_separate_modules = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,8 +54,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
--- docs/index.rst
+++ docs/index.rst
@@ -3,13 +3,18 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to sprig's documentation!
-=================================
+
+
+.. mdinclude:: ../README.md
+
+
+Contents
+--------
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
 
+   modules
 
 
 Indices and tables
