6fc77dce3234f1d7fb38eb971cfa6fed46c6bd9c
AP Ljungquist <ap@ljungquist.eu>
2019-07-09 19:19:54 +0200

    Drops setuptools_scm dependency
    ... because setup_requires is still a pain.

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
+import codecs
 import os
+import re
 
 import setuptools
+
 
 readme_path = os.path.join(os.path.dirname(__file__), 'README.md')
 with open(readme_path, 'r') as fp:
     long_description = fp.read()
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 setuptools.setup(
     name="sprig",
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    version=find_version("src", "sprig", "__init__.py"),
     author="AP Ljungquist",
     author_email="ap@ljungquist.eu",
     description="A home to code that would otherwise be homeless",
